a6abbf4ff898233a934dd32d59b0ebd340daecce
Viktor Stanchev <vikstrous@gmail.com>
2011-08-07 12:32:49 -0400

    made sorting work

--- editor.js
+++ editor.js
@@ -2,6 +2,8 @@ var sort_by_order = function(a, b){
       a.order > b.order;
 };
 
+var max_sort_id = 0;
+
 $(document).ready(function(){
   var LinkData = {};
   
@@ -19,27 +21,29 @@ $(document).ready(function(){
       //field properties
       $field = $('<div>');
       
-      //name input
-      $name_input_label = $('<label>', {'for':'name'+field});
-      $name_input_label.text(this_field.name);
-      if(this_field.multiline){
-        $name_input = $('<textarea>', {id:'name'+field, purpose:'text', field:this_field.name});
-      } else {
-        $name_input = $('<input>', {id:'name'+field, purpose:'text', field:this_field.name});
+      //input builder
+      function build_input(purpose, multiline){
+        var $field_editor = $('<div>');
+        $name_input_label = $('<label>', {'for':'name'+field+purpose});
+        $name_input_label.text(this_field.name + ' ' + purpose);
+        if(multiline){
+          $name_input = $('<textarea>', {id:'name'+field+purpose, purpose:purpose, field:this_field.name});
+        } else {
+          $name_input = $('<input>', {id:'name'+field+purpose, purpose:purpose, field:this_field.name});
+        }
+        $name_input.val(property ? property[purpose] : '');
+        $field_editor.append($name_input_label);
+        $field_editor.append('<br/>');
+        $field_editor.append($name_input);
+        return $field_editor;
       }
-      $name_input.val(property ? property.text : '');
-      $field.append($name_input_label);
-      $field.append($name_input);
+      
+      //text input
+      $field.append(build_input('text', this_field.multiline));
       
       //url input
       if(this_field.url){
-        $field.append('<br/>');
-        $url_input_label = $('<label>', {'for':'url'+field});
-        $url_input_label.text(this_field.name + ' url');
-        $url_input = $('<input>', {id:'url'+field, purpose:'url', field:this_field.name});
-        $url_input.val(property ? property.url : '');
-        $field.append($url_input_label);
-        $field.append($url_input);
+        $field.append(build_input('url', false));
       }
       
       $fields.append($field);
@@ -52,19 +56,23 @@ $(document).ready(function(){
         Save: function(){
             var $inputs = $('input,textarea', $fields);
             var $input, purpose, field, val;
+            
+            //get all the inputs and text areas
             for (var n = 0, length = $inputs.length; n < length; n++){
               $input = $($inputs[n]);//get the input element
               val = $input.val();//new value to set
               field = $input.attr('field');//field to edit
               purpose = $input.attr('purpose');//text or url
               
+              //create the field if it didn't exist but we are giving it a value
               if(entry[field] == undefined && val != ''){
                 entry[field] = {};
               }
+              
+              //set the value
               if(entry[field] != undefined){
                 entry[field][purpose] = val;
               }
-              //console.log(entry[field][purpose]);
             }
             redraw();
             $(this).dialog( "close" );
@@ -76,18 +84,25 @@ $(document).ready(function(){
     });
   }
   
+  //each sortable entry
   function build_entry(index){
     var $a, $edit, property, this_field, $field, fields,
         entry = LinkData.entries[index],
-        $label = $('<div>', {class:'label'}),
-        $entry = $('<li>', {class:"item"}),
+        $label = $('<div>', {class:'label'});
+        
+    entry.sort_id = max_sort_id;
+    
+    var $entry = $('<li>', {class:"item", id:'item_' + max_sort_id++}),
         fields = LinkData.fields.sort(sort_by_order);
     
+    //go through each field and display its value
     for (field in fields){
       this_field = fields[field];
       property = entry[this_field.name];
+      //if this entry has a property coresponding to this field
       if(property != undefined){
         $field = $('<div>', {class: this_field.class + ' field'});
+        //take care of url
         if(property.url){
           $a = $('<a>', {href:property.url});
           $a.text(property.text);
@@ -95,6 +110,7 @@ $(document).ready(function(){
         } else {
           $field.text(property.text);
         }
+        //take care of label
         if(this_field.label){
           $field.prepend($label.clone().text(this_field.label));
         }
@@ -112,16 +128,18 @@ $(document).ready(function(){
     $delete = $('<a>', {class:'faux-button'});
     $delete.text('Delete');
     $delete.click(function(){
+      if(confirm('Are you sure you want to delete this item?')){
         LinkData.entries.splice(index,1);
         redraw();
+      }
     });
     $entry.append($delete);
     return $entry;
   }
   
   function redraw(){
+    max_sort_id = 0;
     var $ul = $('<ul>', {id:'links', class:'grid'});
-    var $li;
     var entries  = LinkData.entries.sort(sort_by_order);
     
     for (entry in entries){
@@ -130,12 +148,18 @@ $(document).ready(function(){
     
     $('#edit-area').empty().append($ul);
 		$ul.sortable({
-			placeholder: "item placeholder"
+			placeholder: "item placeholder",
+      update: function(event, ui){
+        var sort_order = $(this).sortable('toArray');
+        LinkData.entries.sort(function(a, b){
+          return sort_order.indexOf('item_'+a.sort_id) > sort_order.indexOf('item_'+b.sort_id);
+        });
+      }
 		});
 		$ul.disableSelection();
   }
   
-  function init(){
+  function init_list(){
     $('#new-item').click(function(){
       LinkData.entries.push(
         {
@@ -143,7 +167,7 @@ $(document).ready(function(){
             'text' : 'New Item'
           },
           'description': {
-            'text' : 'This item is new'
+            'text' : 'This item is new.'
           }
         }
       );
@@ -158,19 +182,26 @@ $(document).ready(function(){
     $('#refresh').click(function(){
       redraw();
     });
+    $('#reload').click(function(){
+      load_data();
+    });
     redraw();
   }
   
-  $.ajax({
-    url: 'data.js',
-    success: function(data, textStatus, jqXHR){
-      LinkData = data;
-      init();
-    },
-    error: function(jqXHR, textStatus, errorThrown){
-      console.erorr(errorThrown);
-    },
-    dataType: 'json',
-    cache: false
-  });
+  function load_data(){
+    $.ajax({
+      url: 'data.js',
+      success: function(data, textStatus, jqXHR){
+        LinkData = data;
+        init_list();
+      },
+      error: function(jqXHR, textStatus, errorThrown){
+        console.erorr(errorThrown);
+      },
+      dataType: 'json',
+      cache: false
+    });
+  }
+  
+  load_data();
 });
--- editor.php
+++ editor.php
@@ -7,8 +7,15 @@ if($_POST['json']){
 <?php require('pre.php');?>
   <div id="content">
     <div class="bold">TODO: auth, versions, etc.</div>
-    <a id="new-item" class="faux-button">New Item</a><br/>
-    <a id="refresh" class="faux-button">Refresh</a>
+    <a id="refresh" class="faux-button">Refresh</a><br/>
+    <a id="reload" class="faux-button">Reload</a><br/>
+    <div>fields go here</div>
+    <div>name</div>
+    <div>class</div>
+    <div>multiline</div>
+    <div>label</div>
+    <div>url</div>
+    <a id="new-item" class="faux-button">New Item</a>
     <div id="edit-area">
     </div>
     <form id="json_form" action="" method="POST" class="hidden">
