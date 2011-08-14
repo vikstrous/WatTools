1e4f10b67ee06e7786c61f9d062cd6a62a4f9185
Viktor Stanchev <vikstrous@gmail.com>
2011-08-14 12:29:34 -0400

    contact info improvement

--- contact.php
+++ contact.php
@@ -1,6 +1,11 @@
 <?php require('pre.php');?>
   <div id="content">
-    <noscript><div class="noscript">Please enable javascript to view the email address. This is done to avoid spam.</div></noscript>
-    Thank you for your interest in this site. Please send any questions or requests to <script>document.write('wattools'+String.fromCharCode(64)+'gmail.com');</script>.
+    
+    <div>Greetings traveler, <br/>Thank you for your interest in this site. This site is built by <a href="http://viktorstanchev.com">Viktor Stanchev</a>. I believe in openness, so you
+    may take a look at the front end <a href="doc.html">source code with nice comments</a> if you wish. The backend will be
+    closed source for now, but I intend to publish everything on github later on.</div>
+    <div><noscript><div class="noscript">Please enable javascript to view the email address. This is done to avoid spam.</div></noscript>
+    Please send any questions or requests to <script>document.write('wattools'+String.fromCharCode(64)+'gmail.com');</script>.
+    </div>
   </div>
 <?php require('post.php');?>
