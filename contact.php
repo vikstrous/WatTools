cc8d346afe0041dcd353d36974d7c1f3ac643f98
vikstrous <vikstrous@gmail.com>
2011-09-25 11:35:43 -0300

    updated contact

--- contact.php
+++ contact.php
@@ -1,9 +1,7 @@
 <?php require('pre.php');?>
   <div id="content">
     
-    <div>Greetings traveler, <br/>Thank you for your interest in this site. This site is built by <a href="http://viktorstanchev.com">Viktor Stanchev</a>. I believe in openness, so you
-    may take a look at the front end <a href="doc.html">source code with nice comments</a> if you wish. The backend will be
-    closed source for now, but I intend to publish everything on github later on.</div>
+    <div>Greetings traveler, <br/>Thank you for your interest in this site. This site is built by <a href="http://viktorstanchev.com">Viktor Stanchev</a>. All the source code is available on <a href="http://github.com/vikstrous/WatTools">github.com</a></div>
     <div><noscript><div class="noscript">Please enable javascript to view the email address. This is done to avoid spam.</div></noscript>
     Please send any questions or requests to <script>document.write('wattools'+String.fromCharCode(64)+'gmail.com');</script>.
     </div>
