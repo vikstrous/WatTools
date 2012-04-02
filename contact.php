020af977eab34e18b89e71ac1575e0e37fb76475
Viktor Stanchev <vstanchev@tagged.com>
2012-04-02 14:01:42 -0700

    simplify the php, remove pre, post and contact

--- contact.php
+++
@@ -1,9 +0,0 @@
-<?php require('pre.php');?>
-  <div id="content">
-    
-    <div>Greetings traveler, <br/>Thank you for your interest in this site. This site is built by <a href="http://viktorstanchev.com">Viktor Stanchev</a>. All the source code is available on <a href="http://github.com/vikstrous/WatTools">github.com</a></div>
-    <div><noscript><div class="noscript">Please enable javascript to view the email address. This is done to avoid spam.</div></noscript>
-    Please send any questions or requests to <script>document.write('wattools'+String.fromCharCode(64)+'gmail.com');</script>.
-    </div>
-  </div>
-<?php require('post.php');?>
--- index.php
+++ index.php
@@ -1,7 +1,76 @@
 <?php
-require('pre.php');?>
+session_start();
+?><!DOCTYPE HTML>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta property="og:title" content="Waterloo Tools" />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="http://wattools.com" />
+  <meta property="og:image" content="" />
+  <meta property="og:site_name" content="Waterloo Tools" />
+  <meta property="fb:admins" content="100000486272805" />
+  <title>Waterloo Tools - A collection of tools for University of Waterloo students</title>
+  <link rel="alternate" type="application/rss+xml" href="/rss.xml" title="Waterloo Tools" />
+  <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.3.0/build/cssreset/reset-min.css">
+  <link rel="stylesheet" type="text/css" href="css/smoothness/jquery-ui-1.8.14.custom.css" />
+  <link rel="stylesheet" type="text/css" href="css/jquery.jgrowl.css" />
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.15/jquery-ui.min.js"></script>
+  <script type="text/javascript" src="js/lib/jquery.ui.touch.js"></script>
+  <script type="text/javascript" src="js/lib/mustache.js"></script>
+  <script type="text/javascript" src="js/lib/jquery.jgrowl_minimized.js"></script>
+  <script type="text/javascript">
+  $.mustache = function(template, view) {
+    var partials = {}, partial_list = view.partials;
+    //delete view.partials; //we shouldn't modify the view because it's passed by reference
+    if (partial_list instanceof Array) {
+      partial_list.forEach(function(item){
+        partials[item] = $('#'+item+'-tpl').html()
+      });
+    }
+    return Mustache.to_html($('#'+template+'-tpl').html(), view, partials);
+  };
+  </script>
+  <!-- I HATE IE -->
+  <!--[if lte IE 7.0]>
+  <style type="text/css">
+  .grid li {display:inline;}
+  </style>
+  <![endif]--> 
+</head>
+
+<body>
+  <div id="header">
+    <div class='title'><a href="/">Waterloo Tools</a></div>
+    <div class='description'>A collection of tools for University of Waterloo students (made by University of Waterloo students!)</div>
+    <div class='description'>WatTools is now being crowdsourced. Submit your own revisions of the list! Click the Editor button!</div>
+    <div id="social">
+      <div class="social_item">
+        <iframe src="http://www.facebook.com/plugins/like.php?app_id=155945941150696&amp;href=wattools.com&amp;send=false&amp;layout=box_count&amp;width=55&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=90" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:55px; height:90px;" allowTransparency="true"></iframe>
+      </div>
+      <div class="social_item">
+        <g:plusone size="tall" href="http://wattools.com"></g:plusone>
+      </div>
+      <div class="social_item">
+        <a href="http://twitter.com/share" class="twitter-share-button" data-url="http://wattools.com" data-text="I just found some useful tools for #uwaterloo students" data-count="vertical" data-via="wattools">Tweet</a>
+      </div>
+      <div class="clearfix"></div>
+    </div>
+  </div>
+
   <div id="content">
     <div id="app"></div>
+  </div>
+
+  <noscript>
+    Your browser doesn't support JavaScript or you have disabled JavaScript. Sorry, but this app will not work without JavaScript.
+  </noscript>
+
+  <div>Greetings traveler, <br/>Thank you for your interest in this site. This site is built by <a href="http://viktorstanchev.com">Viktor Stanchev</a>. All the source code is available on <a href="http://github.com/vikstrous/WatTools">github.com</a></div>
+  <div><noscript><div class="noscript">Please enable javascript to view the email address. This is done to avoid spam.</div></noscript>
+  Please send any questions or requests to <script>document.write('wattools'+String.fromCharCode(64)+'gmail.com');</script>.
   </div>
 
   <!-- we are preloading some templates and we should be able to get the rest through ajax (currently we preload all) -->
@@ -166,4 +235,25 @@ require('pre.php');?>
     });
   </script>
 
-<?php require('post.php');?>
+  <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-10239938-4']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+    (function() {
+      var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+      po.src = 'https://apis.google.com/js/plusone.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+    })();
+  </script>
+  <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+
+  </body>
+  </html>
