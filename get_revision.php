44d3aa34d38c01569db108d41c08f14e8ae43c21
Viktor Stanchev <me@viktorstanchev.com>
2013-02-04 00:50:23 -0500

    remove lies

--- get_revision.php
+++ get_revision.php
@@ -18,7 +18,6 @@ if(!array_key_exists('revisions', $revisions)) $revisions['revisions'] = array()
 if(!array_key_exists('current', $revisions)) $revisions['current'] = 0;
 
 //did we want a differnet revision for the current one?
-//TODO: fix security hole
 $revision = $revisions['current'];
 if(array_key_exists('revision', $_GET)){
 	$revision = $_GET['revision'];
